00c48229d462fd58daf44493dd0d916a1a4fd6ea
Blair Nangle <hi@blairnangle.com>
2022-12-05 13:41:28 +0000

    Solve Day 1, Part Two

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
-def count_calories(input_file):
+def get_top_elf_calories(input_file):
     with open(input_file) as f:
         lines = f.readlines()
 
@@ -19,4 +19,31 @@ def count_calories(input_file):
     return highest
 
 
-print(count_calories("day1.txt"))
+def get_top_three_elves_calories(input_file):
+    with open(input_file) as f:
+        lines = f.readlines()
+
+    top_three = []
+
+    current = 0
+    n_lines = len(lines)
+
+    for idx, line in enumerate(lines):
+        if line == "\n" or idx == n_lines - 1:
+            if current > min(top_three, default=0):
+                top_three.sort()
+                if len(top_three) == 3:
+                    top_three[0] = current
+                else:
+                    top_three.append(current)
+                current = 0
+            else:
+                current = 0
+        else:
+            current += int(line)
+
+    return sum(top_three)
+
+
+print(f"Solution to Part One: {get_top_elf_calories('day1.txt')}\n")
+print(f"Solution to Part Two: {get_top_three_elves_calories('day1.txt')}")
--- day1_test.py
+++ day1_test.py
@@ -1,5 +1,9 @@
 import day1
 
 
-def test_example_case():
-    assert day1.count_calories("day1_test.txt") == 24000
+def test_example_case_part_one():
+    assert day1.get_top_elf_calories("day1_test.txt") == 24000
+
+
+def test_example_case_part_two():
+    assert day1.get_top_three_elves_calories("day1_test.txt") == 45000
