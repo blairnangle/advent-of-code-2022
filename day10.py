438b2029b8343b4c4d38bdd6b16ab29dca772a14
Blair Nangle <hi@blairnangle.com>
2022-12-14 16:49:26 +0000

    Solve Day 10 (Part Two)

--- day10.py
+++ day10.py
@@ -1,4 +1,6 @@
-def count_strength_at_interesting_cycles(commands: [str], interesting_cycles: [int]) -> int:
+def count_strength_at_interesting_cycles(
+    commands: [str], interesting_cycles: [int]
+) -> int:
     x = 1
     command_idx = 0
     next_cycle_to_apply_command_to = 0
@@ -37,4 +39,58 @@ def count_load_from_file(input_file: str, interesting_cycles: [int]) -> int:
     return count_strength_at_interesting_cycles(commands, interesting_cycles)
 
 
-print(f"Solution to Part One: {count_load_from_file('day10.txt', [20, 60, 100, 140, 180, 220])}\n")
+def draw(commands: [str], crt_width: int, crt_height: int) -> [str]:
+    x = 1
+    command_idx = 0
+    next_cycle_to_apply_command_to = 0
+    command_queue = []
+    crt_row = []
+    row_n = 0
+    monitor = []
+    sprite_position = [0, 1, 2]
+
+    for cycle in range(crt_width * crt_height):
+        pixel_currently_being_drawn = cycle - row_n * crt_width
+        if command_queue:
+            if command_queue[0][0] == cycle:
+                diff = command_queue.pop()[1]
+                x += diff
+                sprite_position = [x - 1, x, x + 1]
+        if pixel_currently_being_drawn in sprite_position:
+            crt_row.append("#")
+        else:
+            crt_row.append(".")
+        if cycle == next_cycle_to_apply_command_to:
+            command = commands[command_idx].strip()
+            if command.startswith("noop"):
+                next_cycle_to_apply_command_to += 1
+                command_idx += 1
+            else:
+                increment = int(command.split(" ")[1])
+                command_queue.append((cycle + 2, increment))
+                next_cycle_to_apply_command_to += 2
+                command_idx += 1
+        if cycle != 0 and (cycle + 1) % crt_width == 0:
+            sprite_position = [0, 1, 2]
+            monitor.append(crt_row)
+            crt_row = []
+            row_n += 1
+
+    return """\n""".join(["".join(row) for row in monitor])
+
+
+def render_from_file(input_file: str, crt_width: int, crt_height: int) -> str:
+    with open(input_file) as f:
+        lines = f.readlines()
+
+    commands = list(map(lambda line: line.strip(), lines))
+
+    return draw(commands, crt_width, crt_height)
+
+
+print(
+    f"Solution to Part One: {count_load_from_file(input_file='day10.txt', interesting_cycles=[20, 60, 100, 140, 180, 220])}\n"
+)
+print(
+    f"Solution to Part Two:\n{render_from_file(input_file='day10.txt', crt_width=40, crt_height=6)}\n"
+)
--- day10_test.py
+++ day10_test.py
@@ -8,15 +8,15 @@ def test_example_case_part_one_list_commands():
         "addx -5",
     ]
 
-    interesting_cycles = [
-        5
-    ]
-
-    assert day10.count_strength_at_interesting_cycles(commands=commands, interesting_cycles=interesting_cycles) == -5
+    assert (
+        day10.count_strength_at_interesting_cycles(
+            commands=commands, interesting_cycles=[5]
+        )
+        == 20
+    )
 
 
 def test_example_case_part_one_from_file():
-
     interesting_cycles = [
         20,
         60,
@@ -26,4 +26,42 @@ def test_example_case_part_one_from_file():
         220,
     ]
 
-    assert day10.count_load_from_file(input_file="day10_test.txt", interesting_cycles=interesting_cycles) == 13140
+    assert (
+        day10.count_load_from_file(
+            input_file="day10_test.txt", interesting_cycles=interesting_cycles
+        )
+        == 13140
+    )
+
+
+def test_example_case_part_two_draw_row_from_list_of_commands():
+    commands = [
+        "addx 15",
+        "addx -11",
+        "addx 6",
+        "addx -3",
+        "addx 5",
+        "addx -1",
+        "addx -8",
+        "addx 13",
+        "addx 4",
+        "noop",
+        "addx -1",
+    ]
+
+    assert (
+        day10.draw(commands=commands, crt_width=21, crt_height=1)
+        == "##..##..##..##..##..#"
+    )
+
+
+def test_example_case_part_two_render_multiple_rows_from_file():
+    assert (
+        day10.render_from_file(input_file="day10_test.txt", crt_width=40, crt_height=6)
+        == """##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######....."""
+    )
